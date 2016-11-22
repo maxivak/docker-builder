3523810a45dd3085aac7a614a836a08c8ce2178d
Max Ivak <max.ivak@galacticexchange.io>
2016-11-22 11:00:20 +0200

    examples

--- examples/example-apps-php/servers/apps-php/cookbooks/apps-php/metadata.rb
+++ examples/example-apps-php/servers/apps-php/cookbooks/apps-php/metadata.rb
@@ -6,4 +6,4 @@ description      'Nginx server with PHP applications'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "apt"
+#depends "apt"
