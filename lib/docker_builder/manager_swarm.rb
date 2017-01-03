9ca4001be0cd879a83826dd76a9dc9cdb0a4df67
Max Ivak <max.ivak@galacticexchange.io>
2017-01-03 18:35:17 +0200

    swarm options

--- lib/docker_builder/manager_swarm.rb
+++ lib/docker_builder/manager_swarm.rb
@@ -24,10 +24,18 @@ module DockerBuilder
 
       run_opts = []
       run_opts << "--network #{docker_opts['swarm_network']}" if docker_opts['swarm_network']
+      run_opts << settings.docker_ports_string
+      run_opts << settings.run_env_variables_string
+
+      # volumes
+      #--mount type=bind,source=/path/on/host,destination=/path/in/container
+      run_opts << settings.docker_volumes.map{|r| "--mount type=bind,source=#{r[0]},destination=#{r[1]}" }.join(' ')
+
+      #
       run_opts << docker_opts['swarm_options']
 
       cmd %Q(docker service create \
---name #{settings.container_name} #{settings.docker_ports_string} \
+--name #{settings.container_name} \
 #{run_opts.join(' ')} \
 #{settings.image_name} #{settings['docker']['command']}
 )
